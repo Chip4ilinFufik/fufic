f00cabf6fd9e469b6e8aef633b28c3f5959d5a6f
Chip4ilinFufik <daniilandronenkov@icloud.com>
2026-04-01 17:25:08 +0300

    Добавлена функция read_files для чтения CSV и подсчета coffee_spent

--- reader.py
+++ reader.py
@@ -3,3 +3,14 @@ import csv
 
 def read_files(file_pats):
     date = []
+    for path in file_paths:
+        try:
+            with open(path, newline="", encoding="utf-8") as f:
+                reader =csv.DictReader(f)
+                for rwo in reader:
+                    row['coffee_spent'] = int(row['coffee_spent'])
+                    date.append(row)
+        except FileNotFoundError:
+            print(f"Файл {path} не найден.")
+
+    return date
