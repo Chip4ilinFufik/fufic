1e5d83e4490ff996d23607ef3861b0265c8f5493
Chip4ilinFufik <daniilandronenkov@icloud.com>
2026-04-01 21:23:34 +0300

    Исправлены ошибки

--- reader.py
+++ reader.py
@@ -1,14 +1,14 @@
 import csv
 
 
-def read_files(file_pats):
+def read_files(file_paths):
     date = []
     for path in file_paths:
         try:
             with open(path, newline="", encoding="utf-8") as f:
-                reader =csv.DictReader(f)
-                for rwo in reader:
-                    row['coffee_spent'] = int(row['coffee_spent'])
+                reader = csv.DictReader(f)
+                for row in reader:
+                    row["coffee_spent"] = int(row["coffee_spent"])
                     date.append(row)
         except FileNotFoundError:
             print(f"Файл {path} не найден.")
