5680a105c0d81069de62015003cfa8727b5de797
Chip4ilinFufik <daniilandronenkov@icloud.com>
2026-04-01 18:22:23 +0300

    Сгрупировал студентов

--- reports/median_coffee.py
+++ reports/median_coffee.py
@@ -2,3 +2,10 @@ from collections import defaultdict
 from statistics import median
 
 from report.base import BaseReport
+
+class MedianCoffeeReport(BaseReport):
+    def generate(self, date):
+
+        for r in data:
+            student = r['student']
+            grouped[student].append(r['coffee_spent'])
