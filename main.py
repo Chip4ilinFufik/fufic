26cd91ccd0d9a498fb0cbf2931218d1322441d14
Chip4ilinFufik <daniilandronenkov@icloud.com>
2026-04-01 21:23:10 +0300

    Исправлены ошибки

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import argparse
-from unittest import result
 from tabulate import tabulate
 from reader import read_files
 from reports.median_coffee import MedianCoffeeReport
 
 REPORTS = {
-    'median_coffee': MedianCoffeeReport,
+    'median-coffee': MedianCoffeeReport,
 }
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--report", nargs="+", required=True)
+    parser.add_argument("--files", nargs="+", required=True)
     parser.add_argument("--report", required=True)
 
     args = parser.parse_args()
 
 
     if args.report not in REPORTS:
-        raise ValueError(f"Неизвестный отчет {args.report}")
+        raise ValueError(f"Неизвестный отчет: {args.report}")
     
-    data - read_files(args.files)
+    data = read_files(args.files)
 
     report_class = REPORTS[args.report]
     report = report_class()
