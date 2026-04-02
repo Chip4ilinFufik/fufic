92d15880333ec46cea1bf3f7010959ceb311508d
Chip4ilinFufik <daniilandronenkov@icloud.com>
2026-04-02 18:33:51 +0300

    Исправлены незначительные ошибки

--- test/test_median_coffee.py
+++ test/test_median_coffee.py
@@ -2,11 +2,12 @@ from reports.median_coffee import MedianCoffeeReport
 
 def test_median_single_student():
     data =[
-        {"student": "Alice", "coffee": 250},
-        {"student": "Bob", "coffee": 300},
-        {"student": "Charlie", "coffee": 200}
+        {"student": "Alice", "coffee_spent": 250},
+        {"student": "Bob", "coffee_spent": 300},
+        {"student": "Charlie", "coffee_spent": 200}
     ]
     report = MedianCoffeeReport()
     result = report.generate(data)
+    
 
-    assert ("alice", 250) in result
+    assert ("Alice", 250) in result
